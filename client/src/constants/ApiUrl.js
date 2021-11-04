c426d61401674e8c9d7cb68fd8b0652eb28bf293
VietPhu-193 <70962981+VietPhu-193@users.noreply.github.com>
2021-11-04 14:58:40 +0700

    node+react+express+mongo

--- client/src/constants/ApiUrl.js
+++ client/src/constants/ApiUrl.js
@@ -1,3 +1,3 @@
-export const URL = process.env.NODE_ENV !== 'production' ? "http://localhost:5000/api" : 'some';
+export const URL = process.env.NODE_ENV !== 'production' ? "http://localhost:5000/api" : 'https://shielded-reaches-05931.herokuapp.com/api';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
